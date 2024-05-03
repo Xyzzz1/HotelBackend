8c2b8925fe4d41b2d732385d7f08df96635a74a1
xyzzz <1534844604@qq.com>
2024-05-03 11:16:31 +0800

    update air conditioner controller

---
+++ src/main/java/com/rabbiter/hotel/controller/user/AirConditionerController.java
@@ -0,0 +1,11 @@
+package com.rabbiter.hotel.controller.user;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/service/conditioner")
+public class AirConditionerController {
+
+}
