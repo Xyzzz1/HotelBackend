29ce8ea28425a194ccb7522a417e9e5d7be30c94
hjqcute <94338855+hjqcute@users.noreply.github.com>
2024-05-08 17:36:14 +0800

    Update PasswordDTO.java

--- src/main/java/com/rabbiter/hotel/dto/PasswordDTO.java
+++ src/main/java/com/rabbiter/hotel/dto/PasswordDTO.java
@@ -1,14 +1,19 @@
 package com.rabbiter.hotel.dto;
 
 /**
- * @author：rabbiter
- * @date：2022/01/03 17:17
+ * @author：hejiaqi
+ * @date：2024/5/8
  * Description：
+ * 该类只有旧密码和新密码两个属性
+ * 用于更换密码时。
+ * 该类使用的时候，应与LoginDTO进行对应使用，即同一个用户的登陆信息和旧密码，新密码信息一致。
+ * 也可将此处的旧密码信息属性增加到LoginDTO，舍弃该类
+ *
  */
 public class PasswordDTO {
 
-    private String oldPassword;
-    private String newPassword;
+    private String oldPassword;//旧密码
+    private String newPassword;//新密码
 
     public PasswordDTO() {
     }
