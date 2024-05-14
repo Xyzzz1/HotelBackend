15e686fbf77176d6e79fee6900e220ad30ffde9b
hjqcute <94338855+hjqcute@users.noreply.github.com>
2024-05-14 19:17:16 +0800

    Update PollQueueTest.java

--- src/main/java/com/rabbiter/hotel/test/unittest/PollQueueTest.java
+++ src/main/java/com/rabbiter/hotel/test/unittest/PollQueueTest.java
@@ -1,18 +1,18 @@
 package com.rabbiter.hotel.test.unittest;
 
-import com.rabbiter.hotel.dto.AirConditionerStatusDTO;
+import com.rabbiter.hotel.dto.AirConditionerUserDTO;
 import com.rabbiter.hotel.dto.QueueDTO;
 import com.rabbiter.hotel.facility.airconditioner.PollQueue;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+
 /**
  * @author hejiaqi
- * @date: 2024/5/5
+ * @date: 2024/5/14
 
  */
-
 public class PollQueueTest {
     public PollQueueTest(){
     }
@@ -20,10 +20,12 @@ public class PollQueueTest {
     @Test
     public void test(){
         QueueDTO dt=new QueueDTO();
+
         dt.setQueueType(1);
-        AirConditionerStatusDTO airConditionerUser1=new AirConditionerStatusDTO(302,1,2,3);
-        AirConditionerStatusDTO airConditionerUser2=new AirConditionerStatusDTO(402,4,5,2);
-        AirConditionerStatusDTO airConditionerUser3=new AirConditionerStatusDTO(502,7,8,1);
+        AirConditionerUserDTO airConditionerUser1=new AirConditionerUserDTO(1,1,2,3);
+        AirConditionerUserDTO airConditionerUser2=new AirConditionerUserDTO(2,4,5,2);
+        AirConditionerUserDTO airConditionerUser3=new AirConditionerUserDTO(3,7,8,8);
+
 
         dt.enqueue(airConditionerUser1);
         dt.enqueue(airConditionerUser2);
@@ -36,8 +38,12 @@ public class PollQueueTest {
         System.out.println("测试服务队列");
         System.out.println(dt.getQueue());
 
-        List<AirConditionerStatusDTO> user1 = PollQueue.getUser1(dt);
-        System.out.println("一次时间片后的结果");
+
+
+        PollQueue pollQueue=new PollQueue();
+        List<AirConditionerUserDTO> user1 = pollQueue.getUser(dt);
+        System.out.println("########################################################");
+        System.out.println("时间片后的返回结果");
         System.out.println(user1.toString());
         System.out.println("DTO中的队列结果");
         System.out.println("服务队列");
@@ -47,8 +53,9 @@ public class PollQueueTest {
         dt.setQueueType(1);
         System.out.println(dt.getQueue());
 
-        user1 = PollQueue.getUser1(dt);
-        System.out.println("二次时间片后的结果");
+        user1 = pollQueue.getUser(dt);
+        System.out.println("########################################################");
+        System.out.println("时间片后的结果");
         System.out.println(user1.toString());
         System.out.println("DTO中的队列结果");
         System.out.println("服务队列");
@@ -58,8 +65,9 @@ public class PollQueueTest {
         dt.setQueueType(1);
         System.out.println(dt.getQueue());
 
-        user1 = PollQueue.getUser1(dt);
-        System.out.println("三次时间片后的结果");
+        user1 = pollQueue.getUser(dt);
+        System.out.println("########################################################");
+        System.out.println("时间片后的结果");
         System.out.println(user1.toString());
         System.out.println("DTO中的队列结果");
         System.out.println("服务队列");
@@ -71,8 +79,35 @@ public class PollQueueTest {
 
 
 
-        user1 = PollQueue.getUser1(dt);
-        System.out.println("四次时间片后的结果");
+        user1 = pollQueue.getUser(dt);
+        System.out.println("########################################################");
+        System.out.println("时间片后的结果");
+        System.out.println(user1.toString());
+        System.out.println("DTO中的队列结果");
+        System.out.println("服务队列");
+        dt.setQueueType(0);
+        System.out.println(dt.getQueue());
+        System.out.println("等待队列");
+        dt.setQueueType(1);
+        System.out.println(dt.getQueue());
+
+
+        user1 = pollQueue.getUser(dt);
+        System.out.println("########################################################");
+        System.out.println("时间片后的结果");
+        System.out.println(user1.toString());
+        System.out.println("DTO中的队列结果");
+        System.out.println("服务队列");
+        dt.setQueueType(0);
+        System.out.println(dt.getQueue());
+        System.out.println("等待队列");
+        dt.setQueueType(1);
+        System.out.println(dt.getQueue());
+
+
+        user1 = pollQueue.getUser(dt);
+        System.out.println("########################################################");
+        System.out.println("时间片后的结果");
         System.out.println(user1.toString());
         System.out.println("DTO中的队列结果");
         System.out.println("服务队列");
